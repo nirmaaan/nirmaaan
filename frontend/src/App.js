2eb0c7f2bc2378d4ce90715636417b8392339a0c
Nirman <nkesari@outlook.com>
2022-04-05 22:37:20 +1000

    static files are served by django server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
+import f1 from './static/f1.jpg'
 
 import Nav from './componenets/nav';
 
 function App() {
+
+  console.log("directory", __dirname)
   return (
     <div className="App">
         <Nav />
+        <img src={f1} width="500px" alt="F1" />
+        
     </div>
   );
 }
