18ea50f7e4f69d16bb5f7afae872da39081e7c8a
Nirman <nkesari@outlook.com>
2022-04-05 22:51:12 +1000

    env variables changed

--- resume/settings.py
+++ resume/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jkgddgs@!1ko8d0%_%5^xd^4duhrf(9&)*hg1nivdj19s3fyjk'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
