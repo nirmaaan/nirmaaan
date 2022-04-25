8e1913c1adf138e5c397f7418b00bacc0ea2582f
Nirman <nkesari@outlook.com>
2022-04-25 19:05:36 +1000

    nav pane mobile completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import './App.css';
+import './menuSymbol.css';
 import { AboutPage, ResumePage, PortfolioPage, LetstalkPage } from './pages/allPages';
+
+import { useState } from 'react';
 
 
 
 
 
 function App() {
-  
+
+  const [hideNav, setHideNav] = useState(true);
 
 
   return (
@@ -20,10 +24,12 @@ function App() {
  
         <div className="main-container">
 
-          {/* lEFT PANE - First Grid Box*/}
+          {/* NAVIGATION */}
+
+          {/* lEFT PANE - First Grid Box - For wide screens only*/}
             <div className="left-pane"> 
 
-                <div className="nav-box">
+                <div className="nav-box center">
                     <a href="/"> About </a><br></br>
                     <a href="/resume"> Resume </a><br></br>
                     <a href="/portfolio"> Portfolio</a><br></br>
@@ -32,12 +38,40 @@ function App() {
 
             </div>
 
-            <div className="nav-pane-mobile"> 
+            {/* NAV PANE MOBILE - CONTENT IN FLEXBOX - For narrow screens only*/}
 
-                <p> MOBILE NAV PANE. </p>
+      
+            <div className="nav-pane-mobile">
+                {/* Show or hide condition*/}
+
+                {/* {`menu-symbol ${hideNav ? "change" : ""}`} */}
+                <div className={`menu-symbol ${hideNav ? "" : "change"}`} onClick={()=> {if (hideNav){ 
+                                                              setHideNav(false)
+                                                            } else{
+                                                              setHideNav(true)}
+                                                            }}
+                                                              >
+                      <div className="bar1"></div>
+                      <div className="bar2"></div>
+                      <div className="bar3"></div>
+                      
+                    </div> 
+
+
+                  <div className={`nav-box ${hideNav ? "hide-nav-box" : ""}`}>
+                  {/* <div className='nav-box'> */}
+                    <a href="/"> About </a><br></br>
+                    <a href="/resume"> Resume </a><br></br>
+                    <a href="/portfolio"> Portfolio</a><br></br>
+                    <a href="/letstalk"> Let's Talk </a><br></br>
+                    </div>
 
             </div>
 
+
+
+
+            {/* PAGE CONTENT */}
 
               <Router>
                 <Routes>
